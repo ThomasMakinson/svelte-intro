a26cf08663d3a64734989108174cc16c7e1c92c7
makitb1 <makitb1@student.op.ac.nz>
2025-09-19 11:24:39 +1200

    Initial commit

---
+++ src/lib/Components/Card.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Picture from '../Components/Picture.svelte';
+    import Details from '../Components/Details.svelte';
+</script>
+<section>
+    <Picture />
+    <Details />
+</section>
+<style>
+    section {
+        display: flex;
+        border: 1px solid black;
+
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Header from '../lib/Components/Header.svelte';
+    import Card from '../lib/Components/Card.svelte';
+</script>
+<div class = "container">
+<h1>Home</h1>
+<Header />
+<main>
+    <Card />
+    <Card />
+</main>
+</div>
